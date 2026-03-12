1b68011c99c7de9f0cb5847767ee8c0dc7c3c032
kacperjanicki <kacperekjanicki9@gmail.com>
2026-03-12 13:27:08 +0100

    Added a simple function to calculate Euclidean Distance between two points

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+static double EuclideanDistance((int, int) a, (int, int) b)
+{
+    double res = Math.Sqrt((Math.Pow(b.Item1 -  a.Item1,2) + Math.Pow(b.Item2 -  a.Item2,2)));
+    Console.WriteLine("Distance between " + a + " and " + b + " is " + res);
+    return res;
+}
+
+(int,int) vec1= (5,2);
+(int,int) vec2= (1,0);
+
+EuclideanDistance(vec1, vec2);
